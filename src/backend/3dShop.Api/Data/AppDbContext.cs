04d98737b7aafdc7236014e39fb89d6db9b006ba
rian.mendes <rian.mendes@iag.puc-rio.br>
2026-05-15 10:18:40 -0300

    feat: implementing db connection test

---
+++ src/backend/3dShop.Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _3dShop.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+    }
+}
--- src/backend/3dShop.Api/Program.cs
+++ src/backend/3dShop.Api/Program.cs
@@ -1,3 +1,7 @@
+using _3dShop.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.WebEncoders.Testing;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var conString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("DefaultConnection string not found.");
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(conString));
+
+var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql(conString)
+                .Options;
+
+//using var teste = new AppDbContext(options);
+try
+{
+    using var teste = new AppDbContext(options);
+    teste.Database.OpenConnection(); // força a abertura
+    Console.WriteLine("Conexão OK!");
+    teste.Database.CloseConnection();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro ao conectar: " + ex.Message);
+}
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
