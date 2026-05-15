75bacdf031572da1665bb4e4e27d092f02da49fb
devrianmendes <dev.rianmendes@outlook.com>
2026-05-15 18:24:55 -0300

    feat: add category configuration

---
+++ src/backend/3dShop.Api/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using _3dShop.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _3dShop.Api.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("categories");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.NamePt)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(c => c.NameEn)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(u => u.CreatedAt)
+                .IsRequired();
+
+            builder.Property(u => u.UpdatedAt)
+                .IsRequired();
+
+            builder.HasMany(c => c.ProductList)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+                
+
+        }
+    }
+}
