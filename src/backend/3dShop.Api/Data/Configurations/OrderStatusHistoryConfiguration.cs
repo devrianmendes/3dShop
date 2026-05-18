3e89b7d6c1c773e2706eb5613d88dd8e9e9ae79e
rian.mendes <rian.mendes@iag.puc-rio.br>
2026-05-18 16:30:34 -0300

    feat: WIP orderstatushistory configuration

---
+++ src/backend/3dShop.Api/Data/Configurations/OrderStatusHistoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using _3dShop.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _3dShop.Api.Data.Configurations
+{
+    public class OrderStatusHistoryConfiguration : IEntityTypeConfiguration<OrderStatusHistory>
+    {
+        public void Configure(EntityTypeBuilder<OrderStatusHistory> builder)
+        {
+            builder.HasKey(os => os.Id);
+
+            builder.Property(os => os.FromStatus)
+                .HasMaxLength(30)
+                .HasColumnType("VARCHAR(30)");
+
+            builder.HasOne(os => os.Order)
+                .WithMany(o => o.OrderStatusHistory)
+                .HasForeignKey(os => os.OrderId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_OrderStatusHistory_Order_OrderId")
+        
+                    
+        }
+
+    }
+}
