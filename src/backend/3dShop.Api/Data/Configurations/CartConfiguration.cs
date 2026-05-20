beb1bf1358db5288a3e6067545cf92307ea85663
rian.mendes <rian.mendes@iag.puc-rio.br>
2026-05-20 09:23:27 -0300

    feat: cartConfiguration

---
+++ src/backend/3dShop.Api/Data/Configurations/CartConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using _3dShop.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _3dShop.Api.Data.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("carts");
+
+            builder.HasKey(c => c.Id);
+
+            builder.HasIndex(c => c.CustomerId)
+                .IsUnique();                
+
+            builder.HasOne(c => c.Customer)
+                .WithOne(u => u.Cart)
+                .HasForeignKey<Cart>(c => c.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("FK_Cart_User_UserId");
+
+            builder.Property(c => c.CreatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp");
+
+            builder.Property(c => c.UpdatedAt)
+                .IsRequired()
+                .HasColumnType("timestamp");
+
+            builder.Navigation(c => c.Customer).AutoInclude(false);
+            builder.Navigation(c => c.CartItemList).AutoInclude(false);
+        }
+    }
+}
